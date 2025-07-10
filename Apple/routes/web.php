b63c64d4e171c6e62564d7a06bcb745595f1be18
Iskander <sopyyewiskander@gmail.com>
2025-07-11 00:46:26 +0500

    1.0.2

--- Apple/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-
-
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
